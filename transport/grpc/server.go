47f84a5e8d235c99baa07094cb73627312604b9e
Mohammad Safakhou <mohammadsafakhou@pop-os.localdomain>
2020-09-14 00:22:00 +0430

    fix subscription issues

--- transport/grpc/server.go
+++ transport/grpc/server.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"fmt"
 	"github.com/hermeschat/proto"
+	"github.com/nats-io/stan.go"
 	"google.golang.org/grpc"
 	"net"
 	"pusher/transport/nats"
@@ -24,36 +25,54 @@ func CreateGRPCServer() {
 
 type PusherService struct {
 	proto.UnstablePusherService
+}
+
+type ClientProperty struct {
 	mh nats.MessageHandler
+	ssub stan.Subscription
+	subject string
 }
 
 func (ps *PusherService) PusherEventBuff(server proto.Pusher_PusherEventBuffServer) error {
 	// TODO client id must change into user id or sth from user identifier
 	fmt.Println("new user connected")
-	nconn, err := nats.InitMessageHandler("test-cluster", "change_this")
-	if err != nil {
-		return err
-	}
 
-	ps.mh = nconn
+	cp := new(ClientProperty)
 	for {
 		event, err := server.Recv()
 		if err != nil {
 			continue
 		}
 		if _, msg := event.Event.(*proto.PusherEvent_Client); msg {
-			err = ps.mh.StreamPublisher(event.GetClient().ToSub, event.GetClient().Data)
+			if cp.mh.IsConnected() != true {
+				err := server.SendMsg("you have to join first")
+				if err != nil {
+
+				}
+				continue
+			}
+			myBadge := []byte(cp.subject + "::")
+			err = cp.mh.StreamPublisher(event.GetClient().ToSub, append(myBadge, event.GetClient().Data...))
 			if err != nil {
 				// handle errors in here in better way
 			}
 			fmt.Println("published")
 			continue
 		} else if _, msg := event.Event.(*proto.PusherEvent_Join); msg {
+			nconn, err := nats.InitMessageHandler("test-cluster", event.GetJoin().MySub)
+			if err != nil {
+				return err
+			}
+			cp.mh = nconn
 			// TODO handle subscriber variable maybe store address or cache it
-			_, err := ps.mh.StreamSubscriber(event.GetJoin().MySub, server)
+			mysub := event.GetJoin().MySub
+
+			sub, err := cp.mh.StreamSubscriber(mysub, server)
 			if err != nil {
 				// TODO handle error
 			}
+			cp.ssub = sub
+			cp.subject = mysub
 			fmt.Println("joined")
 		} else {
 			// TODO send not supported event
--- transport/nats/init.go
+++ transport/nats/init.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"github.com/hermeschat/proto"
 	"github.com/nats-io/stan.go"
+	"strings"
 )
 
 type MessageHandler interface {
 	StreamSubscriber(subject string, g proto.Pusher_PusherEventBuffServer) (stan.Subscription, error)
 	StreamPublisher(subject string, msg []byte) error
+	StreamUnsubscribe(sub stan.Subscription) error
+	IsConnected() bool
 }
 
 type tempStructNameNats struct {
@@ -28,12 +31,16 @@ func StanStreamConnection(clusterId string, clientId string) (stan.Conn, error) 
 }
 
 func (c *tempStructNameNats) StreamSubscriber(subject string, g proto.Pusher_PusherEventBuffServer) (stan.Subscription, error) {
-	fmt.Println("subscribe started")
+	fmt.Println("subscribe is starting with " + subject)
 	sub, err := c.Subscribe(subject, func(msg *stan.Msg) {
 		fmt.Println("subscriber received message")
+		data := string(msg.Data)
+		temp := strings.SplitN(data, "::", 2)
+		mysub := temp[0]
+		data = temp[1]
 		_ = g.Send(&proto.PusherEvent{Event: &proto.PusherEvent_Server{Server: &proto.Server{
-			FromSub: msg.Subject,
-			Data:    msg.Data,
+			FromSub: mysub,
+			Data:    []byte(data),
 		}}})
 		// TODO error need to be handled in a reasonable way
 		// maybe cache it or send it to a worker for handling it
@@ -51,6 +58,10 @@ func (c *tempStructNameNats) StreamPublisher(subject string, msg []byte) error {
 	return c.Publish(subject, msg)
 }
 
-func StreamUnsubscribe(sub stan.Subscription) error {
+func (c *tempStructNameNats) StreamUnsubscribe(sub stan.Subscription) error {
 	return sub.Unsubscribe()
+}
+
+func (c *tempStructNameNats) IsConnected() bool {
+	return c.NatsConn().IsConnected()
 }
